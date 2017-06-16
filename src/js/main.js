f5e1f5d10065958d5064925fe68822277b624e13
DoubleJ <jerry7722@hotmail.com>
2017-06-16 14:54:29 +0800

    update js

--- src/js/main.js
+++ src/js/main.js
@@ -31,41 +31,26 @@ window.addEventListener('DOMContentLoaded', function (){
 
   // Start OF: Scroll show/hide navbar  =====
   var navigation = (function(){
-    var currentScroll = $(window).scrollTop();
-    if (currentScroll > 70) {
-      defaultNav();
-      $(window).scroll(function(){
-        if ($(this).scrollTop() > 70) {
-          defaultNav();
-        } else {
-          hideNav();
-          removedefaultNav();
-        }
+    var bind = function () {
+      var currentTop = $(window).scrollTop();
+      $(window).scroll(function () {
+        var currentTop = $(this).scrollTop();
+        var timer = window.setTimeout(function() {
+          if (currentTop > 70) {
+            $("[data-nav-status='toggle']").addClass('is-show');
+          } else {
+            $("[data-nav-status='toggle']").removeClass('is-show');
+            $("[data-nav-status='toggle']").removeClass('is-default');
+          };
+        }, 200);
       });
-    } else {
-      removedefaultNav();
-      $(window).scroll(function(){
-        if ($(this).scrollTop() > 70) {
-          showNav();
-        } else {
-          hideNav();
-          removedefaultNav();
-        }
-      });
+      if (currentTop > 70) {
+        $("[data-nav-status='toggle']").addClass('is-default');
+      };
+    };
+    return {
+      bind: bind
     }
-    function showNav() {
-      $("[data-nav-status='toggle']").addClass('is-show');
-    };
-    function hideNav() {
-      $("[data-nav-status='toggle']").removeClass('is-show');
-    };
-    function defaultNav() {
-      $("[data-nav-status='toggle']").addClass('is-default');
-    };
-    function removedefaultNav() {
-      $("[data-nav-status='toggle']").removeClass('is-default');
-    };
-
   }());
   // END OF: Scroll show/hide navbar  =====
 
@@ -127,6 +112,7 @@ window.addEventListener('DOMContentLoaded', function (){
     var $contentTrigger = $('.js-content-trigger');
     var $contentBox = $('.js-content-box');
     var slidingTime = 400;
+
     var bind = function () {
       $contentTrigger.on('click', function(event) {
         event.preventDefault();
@@ -163,103 +149,96 @@ window.addEventListener('DOMContentLoaded', function (){
     $(window).resize(function() {
       windowSize();
     });
-
-    con = canvas[0].getContext('2d');
-    for (var i = 0; i < 40; i++) {
-      pxs[i] = new Circle();
-      pxs[i].reset();
-    }
-    requestAnimationFrame(draw);
-
-    function draw() {
-      con.clearRect(0, 0, WIDTH, HEIGHT);
-      con.globalCompositeOperation = "lighter";
-      for (var i = 0; i < pxs.length; i++) {
-        pxs[i].fade();
-        pxs[i].move();
-        pxs[i].draw();
+    var init = function () {
+      con = canvas[0].getContext('2d');
+      for (var i = 0; i < 40; i++) {
+        pxs[i] = new Circle();
+        pxs[i].reset();
       }
       requestAnimationFrame(draw);
-    }
 
-    function Circle() {
-      this.s = {
-        ttl    : 15000,
-        xmax   : 3,
-        ymax   : 2,
-        rmax   : 16,
-        rt     : 1,
-        xdef   : 960,
-        ydef   : 540,
-        xdrift : 4,
-        ydrift : 4,
-        random : true,
-        blink  : true
+      function draw() {
+        con.clearRect(0, 0, WIDTH, HEIGHT);
+        con.globalCompositeOperation = "lighter";
+        for (var i = 0; i < pxs.length; i++) {
+          pxs[i].fade();
+          pxs[i].move();
+          pxs[i].draw();
+        }
+        requestAnimationFrame(draw);
+      }
+
+      function Circle() {
+        this.s = {
+          ttl    : 15000,
+          xmax   : 3,
+          ymax   : 2,
+          rmax   : 16,
+          rt     : 1,
+          xdef   : 960,
+          ydef   : 540,
+          xdrift : 4,
+          ydrift : 4,
+          random : true,
+          blink  : true
+        };
+        this.reset = function() {
+          this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
+          this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
+          this.r = ((this.s.rmax - 1) * Math.random()) + 1;
+          this.dx = (Math.random() * this.s.xmax) * (Math.random() < 0.5 ? -1 : 1);
+          this.dy = (Math.random() * this.s.ymax) * (Math.random() < 0.5 ? -1 : 1);
+          this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
+          this.rt = Math.random() * this.hl;
+          this.s.rt = Math.random() + 1;
+          this.stop = Math.random() * 0.2 + 0.4;
+          this.s.xdrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+          this.s.ydrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
+        };
+        this.fade = function() {
+          this.rt += this.s.rt;
+        };
+        this.draw = function() {
+          if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt * -1;
+          else if (this.rt >= this.hl) this.reset();
+          var newo = 1 - (this.rt / this.hl);
+          con.beginPath();
+          con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+          con.closePath();
+          var cr = this.r * newo;
+          g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
+          g.addColorStop(0.0, 'rgba(255,255,255,' + newo + ')');
+          g.addColorStop(this.stop, 'rgba(255,255,255,' + (newo * 0.2) + ')');
+          g.addColorStop(1.0, 'rgba(255,255,255,0)');
+          con.fillStyle = g;
+          con.fill();
+        };
+        this.move = function() {
+          this.x += (this.rt / this.hl) * this.dx;
+          this.y += (this.rt / this.hl) * this.dy;
+          if (this.x > WIDTH || this.x < 0) this.dx *= -1;
+          if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+        };
+        this.getX = function() {
+          return this.x;
+        };
+        this.getY = function() {
+          return this.y;
+        };
       };
-      this.reset = function() {
-        this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
-        this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
-        this.r = ((this.s.rmax - 1) * Math.random()) + 1;
-        this.dx = (Math.random() * this.s.xmax) * (Math.random() < 0.5 ? -1 : 1);
-        this.dy = (Math.random() * this.s.ymax) * (Math.random() < 0.5 ? -1 : 1);
-        this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
-        this.rt = Math.random() * this.hl;
-        this.s.rt = Math.random() + 1;
-        this.stop = Math.random() * 0.2 + 0.4;
-        this.s.xdrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
-        this.s.ydrift *= Math.random() * (Math.random() < 0.5 ? -1 : 1);
-      };
-      this.fade = function() {
-        this.rt += this.s.rt;
-      };
-      this.draw = function() {
-        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt * -1;
-        else if (this.rt >= this.hl) this.reset();
-        var newo = 1 - (this.rt / this.hl);
-        con.beginPath();
-        con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
-        con.closePath();
-        var cr = this.r * newo;
-        g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
-        g.addColorStop(0.0, 'rgba(255,255,255,' + newo + ')');
-        g.addColorStop(this.stop, 'rgba(255,255,255,' + (newo * 0.2) + ')');
-        g.addColorStop(1.0, 'rgba(255,255,255,0)');
-        con.fillStyle = g;
-        con.fill();
-      };
-      this.move = function() {
-        this.x += (this.rt / this.hl) * this.dx;
-        this.y += (this.rt / this.hl) * this.dy;
-        if (this.x > WIDTH || this.x < 0) this.dx *= -1;
-        if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
-      };
-      this.getX = function() {
-        return this.x;
-      };
-      this.getY = function() {
-        return this.y;
-      };
-    };
+    }
+    return {
+      init: init
+    }
   }());
   // END OF: Galaxy canvas animation =====
 
   fonts.load();
-  function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-      clearTimeout(timeout);
-      timeout = setTimeout(func, wait);
-    };
-  };
-  function realFunc(){
-    navigation;
-  }
-  window.addEventListener('scroll',debounce(realFunc,500));
-  window.addEventListener('scroll',realFunc);
   contentChanger.bind();
   if($('.filtr-container').length > 0){
     filterizr.init();
-  }
-  galaxy;
+  };
+  navigation.bind();
+  galaxy.init();
 });
 // == END OF CEPAVE JS ==
